5dad4c27acec58b932f0dcae5309475e1256baf1
Frederic Fondement <frederic.fondement@uha.fr>
2023-06-22 14:35:05 +0200

    Real test

--- src/test/java/fr/uha/ensisa/antidemo/IT/AntidemoIT.java
+++ src/test/java/fr/uha/ensisa/antidemo/IT/AntidemoIT.java
@@ -2,7 +2,8 @@ package fr.uha.ensisa.antidemo.IT;
 
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import java.util.*;
 
 import fr.uha.ensisa.eco.metrologie.extension.EcoExtension;
 import fr.uha.ensisa.eco.metrologie.extension.annotations.EcoDocker;
@@ -12,16 +13,44 @@ import fr.uha.ensisa.eco.metrologie.extension.annotations.EcoMonitor;
 import fr.uha.ensisa.eco.metrologie.extension.annotations.EcoWebDriver;
 
 @EcoDocker(network = "metrologie-network", url = "$DOCKER_HOST$")
-@EcoDockerContainer(id = "$NGINX_PROXY_ID$", port=8082)
+@EcoDockerContainer(id = "anti-demo-proxy-1", port=8082)
+@EcoMonitor(containerId = "anti-demo-app-1")
+@EcoMonitor(containerId = "anti-demo-db-1")
 @EcoGatling(userCount = 20)
 @EcoWebDriver(remote = true)
 @ExtendWith(EcoExtension.class)
 public class AntidemoIT {
+    private Integer NOMBRE_ARTICLE_TEST = 2;
+    //en secondes
+    private int TEMPS_PAR_ARTICLE = 10;
+    private int TEMPS_RECHERCHE_ARTICLE = 5;
     
     @RepeatedTest(3)
-    void testIndex(WebDriver driver) {
-        driver.get("/");
-        System.out.println("Hello from testIndex");
-        System.out.println(driver.getCurrentUrl());
+    void testIndex(WebDriver wb) {
+        JavascriptExecutor js = (JavascriptExecutor)wb;
+        wb.get("/");
+        int nb;
+        try {
+            Random rnd = new Random();
+            js.executeScript("window.scrollBy(-2000,0)", "");
+            Thread.sleep(TEMPS_RECHERCHE_ARTICLE*1000);
+            for(int i = 0; i< NOMBRE_ARTICLE_TEST; i++){
+
+                List<WebElement> articles = wb.findElements(By.className("danger-btn"));
+                nb=rnd.nextInt(articles.size());
+                articles.get(nb).click();
+                Thread.sleep(TEMPS_PAR_ARTICLE*300);
+                js.executeScript("window.scrollTo(0,document.body.scrollHeight)", "");
+                Thread.sleep(TEMPS_PAR_ARTICLE*500);
+                js.executeScript("window.scrollTo(0,0)", "");
+                Thread.sleep(TEMPS_PAR_ARTICLE*200);
+                wb.get("/");
+                js.executeScript("window.scrollBy(-2500,0)", "");
+                Thread.sleep(TEMPS_RECHERCHE_ARTICLE*1000);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
